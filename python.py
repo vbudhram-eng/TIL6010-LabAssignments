28d39822480a1c20d2149bc796ee8cf487a563da
Ivaylo Iliev <64080096+Ivo-harry@users.noreply.github.com>
2024-10-02 12:32:05 +0200

    changed the python

--- python.py
+++ python.py
@@ -1,12 +1,8 @@
 import pandas as pd
 
 # Define the file path
-file_path = 'C:/Users/MSI/Desktop/downloaded files from uni/Year 4/Q1/TIL6022 TIL Python Programming/New folder/Dataset_Passenger_data.xlsx'
-file_path_2 = 'C:/Users/MSI/Desktop/downloaded files from uni/Year 4/somehting.x'
-
+file_path = 'C:/Users/MSI/Desktop/downloaded files from uni/Year 4/Q1/TIL6022 TIL Python Programming/New folder/dataset_full.csv'
 
 # Read the CSV file
-df = pd.read_excel(file_path)
+df = pd.read_csv(file_path, delimiter=";")
 print(df.head())
-
-print("Test inside github itself")
